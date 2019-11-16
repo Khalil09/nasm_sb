4bc42253aff9ea710e4f8f10330860e6bf024419
streeg <140141961@aluno.unb.br>
2019-11-16 15:22:15 -0300

    lfsr working for 256

--- LFSR/src/lfsr.c
+++ LFSR/src/lfsr.c
@@ -1,42 +1,89 @@
-#include "../header/lfsr.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#define NN 256
+#define SEED 0x8016
+#define MASK 0x00FFFFFF
 
-uint16_t shift_lsfr(uint16_t *lfsr, uint16_t mask){
-    int feedback;
+//vetor de classes
+int classes[NN];
+double chiQuad[NN];
+double distChi = 0;
 
-    feedback = *lfsr & 1;
-    *lfsr >>= 1;
-    if (feedback == 1) {
-        *lfsr ^= mask;
-    }
-    return *lfsr;
+//declarações de funções
+void inicializaClasses();
+void inicializaChiQuad();
+void calcFreq();
+void separaClasses(uint32_t);
+void lfsr();
+
+//corpo das funções
+void inicializaClasses(){
+  for (int i = 0;  i < NN; i++) {
+    classes[i] = 0;
+  }
 }
 
-void init_lfsrs() {
-    lfsr32 = 0xABCDE;
-    lfsr31 = 0x23456789;
+void inicializaChiQuad() {
+  for (int i = 0;  i < NN; i++) {
+    chiQuad[i] = 0;
+  }
+ }
+
+void separaClasses(uint32_t a) {
+  uint32_t aux = (a/NN);
+  classes[aux]++;
 }
 
-uint16_t get_random() {
-    shift_lsfr(&lfsr32, POLY_MASK_32);
-    return (shift_lsfr(&lfsr32, POLY_MASK_32) ^ shift_lsfr(&lfsr31, POLY_MASK_31)) & 0xffff;
+void calcFreq(){
+  double aux =0;
+
+  for (int i = 0;  i < NN; i++) {
+    aux = (pow((classes[i]-NN),2))/NN;
+    chiQuad[i] = aux;
+
+    distChi += chiQuad[i];
+  }
+}
+/*
+função que gera os números pseudo aleatórios adaptada de:
+https://en.wikipedia.org/wiki/Linear-feedback_shift_register
+*/
+void lfsr(){
+  uint32_t start_state = SEED;  /* Any nonzero start state will work. */
+  uint32_t lfsr = start_state;
+  uint32_t bit;                    /* Must be 16bit to allow bit<<15 later in the code */
+  unsigned period = 0;
+  unsigned long int contador = 0;
+
+  do
+  {
+      /* taps: 16 14 13 11; feedback polynomial: x^16 + x^14 + x^13 + x^11 + 1 */
+      bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ^ (lfsr >> 7)  ^ (lfsr >> 13) ^ (lfsr >> 17)) & 1;
+      lfsr =  (lfsr >> 1) | (bit << 15);
+      ++period;
+      contador++;
+      lfsr = lfsr & MASK;
+      separaClasses(lfsr);
+  } while (/*lfsr != start_state && */contador != (NN*NN-1));
+
+  printf("\n\n\t gerou %lu números\n",contador);
+
+
+
 }
 
-uint16_t * get_random_array() {
-    static uint16_t r[10];
+int main(void){
+  inicializaChiQuad();
+  inicializaClasses();
+  lfsr();
+  calcFreq();
+  for (int i = 0; i < NN; i++) {
+    printf("classe %d tem %d elementos e o valor chi da classe %d é %lf\n",i,classes[i],i,chiQuad[i]);
+  }
 
-    for (int i = 0; i < 10; ++i) {
-      r[i] = get_random();
-   }
-}
+  printf("\tvalor chi quadrado é %lf\n", distChi);
 
-int main() {
-    uint16_t random_value;
-    uint16_t *p;
-    init_lfsrs();
-    random_value = get_random();
-    
-    p = get_random_array();
-    for ( int i = 0; i < 10; i++ ) {
-      printf("%d\n", p[i]);
-   }
+  getchar();
+  return 0;
 }
